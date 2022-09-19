c27981a62983f0a1e5ba166fcccec62f00b97618
azPeee <taprickk2@gmail.com>
2022-09-19 18:20:12 +0200

    commented bugs

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' // (", nem  ' lehet igy is jo)     <<std::endl
+    for (int i = 0;) // nincs leptetes (int i=1;i<=N_ELEMENTS;i++)
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //b[i-1]
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //i<N_ELEMENTS
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"  //<<b[i]<<std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //double atlag=0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
