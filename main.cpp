26ccdbf937147966690596191999d1657442eb60
azPeee <taprickk2@gmail.com>
2022-09-19 18:23:26 +0200

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // (", nem  ' lehet igy is jo)     <<std::endl
-    for (int i = 0;) // nincs leptetes (int i=1;i<=N_ELEMENTS;i++)
+    int *b = new int[N_ELEMENTS]; //N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // (", nem  ' lehet igy is jo)     <<std::endl
+    for (int i = 1;i<=N_ELEMENTS;i++) // nincs leptetes (int i=1;i<=N_ELEMENTS;i++)
     {
-        b[i] = i * 2; //b[i-1]
+        b[i-1] = i * 2; //b[i-1]
     }
-    for (int i = 0; i; i++) //i<N_ELEMENTS
+    for (int i = 0; i<N_ELEMENTS; i++) //i<N_ELEMENTS
     {
-        std::cout << "Ertek:"  //<<b[i]<<std::endl;
+        std::cout << "Ertek: "<<b[i]<<std::endl;  //<<b[i]<<std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //double atlag=0;
+    double atlag=0; //double atlag=0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
